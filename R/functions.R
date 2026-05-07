2cb26121e182d3896867b15d0eaa359854586f6b
David Vadsholt <david.vadsholt@gmail.com>
2026-05-07 12:04:46 +0200

    Made a full join in clean document

--- R/functions.R
+++ R/functions.R
@@ -3,11 +3,11 @@
 #' @param file_path path to datafile
 #'
 #' @returns dataset
-read <- function(file_path, max_rows = 10) {
+read <- function(file_path, max_rows = 100) {
   data <- file_path %>%
     readr::read_csv(
       show_col_types = FALSE,
-      name_repair = to_snake_case,
+      name_repair = snakecase::to_snake_case,
       n_max = max_rows,
     )
   return(data)
@@ -27,7 +27,7 @@ read_all <- function(filename) {
 get_participant_id <- function(data, mutate) {
   data_with_id <- data %>%
     dplyr::mutate(
-      id = str_extract(
+      id = stringr::str_extract(
         file_path_id,
         pattern = "/stress/[:alnum:]{2}/"
       ) %>%
